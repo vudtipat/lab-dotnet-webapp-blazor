dea4746edf94b893e72a25863dd3a1c420417d31
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-04 23:36:24 +0700

    add mocking data and models

---
+++ demowebapp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace demowebapp.Models
+{
+	public class Product
+	{
+		public string Id { get; set; } = null!;
+		public string Maker { get; set; } = null!;
+        [JsonPropertyName("img")]
+        public string Image { get; set; } = null!;
+        public string Url { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public int[]? Rating { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);    
+    }
+}
+
