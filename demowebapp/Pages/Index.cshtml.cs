e2c67f6ee0c4f569a76ed2240cdd71eb14a3eec7
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-04 23:41:38 +0700

    add read mock data service and use data to render

--- demowebapp/Pages/Index.cshtml.cs
+++ demowebapp/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using demowebapp.Models;
+using demowebapp.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace demowebapp.Pages;
@@ -6,15 +8,19 @@ namespace demowebapp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public JsonFileProductService _productService;
+    public IEnumerable<Product> Products { get; private set; } = null!;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger,
+        JsonFileProductService jsonFileProductReader)
     {
         _logger = logger;
+        _productService = jsonFileProductReader;
     }
 
     public void OnGet()
     {
-
+        Products = _productService.GetProducts();
     }
 }
 
---
+++ demowebapp/Services/JsonFileProductService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using demowebapp.Models;
+
+namespace demowebapp.Services
+{
+	public class JsonFileProductService
+	{
+		public IWebHostEnvironment _webHostEnvironment { get; }
+
+		public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+		{
+			_webHostEnvironment = webHostEnvironment;
+        }
+
+		private string JsonFileName
+		{
+			get { return Path.Combine(_webHostEnvironment.WebRootPath, "data", "products.json"); }
+		}
+
+		public IEnumerable<Product> GetProducts()
+		{
+			using (var jsonFileReader = File.OpenText(JsonFileName))
+			{
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+					new JsonSerializerOptions
+					{
+						PropertyNameCaseInsensitive = true
+					});
+			}
+		}
+	}
+}
+
