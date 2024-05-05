730225da032ec4f0cb39c2a28ec39d78fd0fc746
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-05 12:13:37 +0700

    add blazor component

--- demowebapp/Program.cs
+++ demowebapp/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddTransient<JsonFileProductService>();
 
 var app = builder.Build();
@@ -25,8 +26,8 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
 app.MapControllers();
+app.MapBlazorHub();
 
 app.Run();
 
